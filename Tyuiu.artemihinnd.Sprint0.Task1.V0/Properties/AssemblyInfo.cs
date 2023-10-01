4863000330fa18951d494a94e74824cbdc71fad5
Tyuiu.artemihinnd@outlook.com <nikitos.artemihin@mail.ru>
2023-10-01 21:05:04 +0500

    Task1

--- Tyuiu.artemihinnd.Sprint0.Task1.V0/Properties/AssemblyInfo.cs
+++ Tyuiu.artemihinnd.Sprint0.Task1.V0/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tyuiu.artemihinnd.Sprint0.Task0")]
+[assembly: AssemblyTitle("Tyuiu.artemihinnd.Sprint0.Task1.V0")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tyuiu.artemihinnd.Sprint0.Task0")]
+[assembly: AssemblyProduct("Tyuiu.artemihinnd.Sprint0.Task1.V0")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("36adf2fc-f033-4a35-85bc-a9e913c81565")]
+[assembly: Guid("4581293a-2486-41ca-bf5f-dd8d977f4712")]
 
 // Version information for an assembly consists of the following four values:
 //
